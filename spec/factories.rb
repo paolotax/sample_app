ddd46d02e2db35e1b3c460c97e1fbae6e659aa55
Paolo Tassinari <paolo.tassinari@gmail.com>
2010-12-19 11:55:27 +0100

    Added User page with gravatar

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Paolo Tax"  
+  user.email                 "paolo@example.com"  
+  user.password              "secret"             
+  user.password_confirmation "secret"
+end             
