6428c7455d309c3f56221c7623a82f02eb448d7e
Paolo Tassinari <paolo.tassinari@gmail.com>
2010-12-21 15:17:01 +0100

    Added admin migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20101218174401
+# Schema version: 20101221135149
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
