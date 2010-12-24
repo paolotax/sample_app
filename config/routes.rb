8c75df05736446fe0db4ec2ce08631885efa4426
Paolo Tassinari <paolo.tassinari@gmail.com>
2010-12-24 18:56:16 +0100

    Added Ajax methods for follow/unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ SampleApp::Application.routes.draw do
   
   resources :sessions, :only => [ :new, :create, :destroy ]  
   resources :microposts, :only => [:create, :destroy] 
-  resources :relationships, :only => [:create, :destroy, :show] 
+  resources :relationships, :only => [:create, :destroy] 
   
   root :to => 'pages#home'
   
